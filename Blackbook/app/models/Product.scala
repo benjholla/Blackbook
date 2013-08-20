732bf1095ba328eaa58fa38c4a88a570f1a544b6
Fifty-Nine <tim.prince@gmail.com>
2013-08-19 19:49:28 -0500

    FOR REAL.

--- Blackbook/app/models/Product.scala
+++ Blackbook/app/models/Product.scala
@@ -5,6 +5,8 @@ import anorm.SqlParser._
 import play.api.db._
 import play.api.Play.current
 
+import scala.language.postfixOps
+
 case class Product(id: Long, label: String)
 
 object Product {
--- Blackbook/app/models/Tag.scala
+++ Blackbook/app/models/Tag.scala
@@ -6,6 +6,8 @@ import anorm.SqlParser._
 import play.api.db._
 import play.api.Play.current
 
+import scala.language.postfixOps
+
 case class Tag(id: Long, label: String)
 
 object Tag {
