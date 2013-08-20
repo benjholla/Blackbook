4efe65d7b4b7745b00a6435e84cc3b814fdc3297
Ben Holland <benjholla@gmail.com>
2013-08-19 18:43:10 -0700

    I don't give a damn 'bout my reputation

--- Blackbook/conf/evolutions/default/4.sql
+++ Blackbook/conf/evolutions/default/4.sql
@@ -2,14 +2,11 @@
 
 # --- !Ups
 
-ALTER TABLE Products DROP label;
-ALTER TABLE Products ADD name varchar(255);
-
-ALTER TABLE Tags DROP label;
-ALTER TABLE Tags ADD name varchar(255);
+ALTER TABLE Products ALTER COLUMN label RENAME TO name;
+ALTER TABLE Tags ALTER COLUMN label RENAME TO name;
 
 # --- !Downs
 
-ALTER TABLE Products DROP name;
-ALTER TABLE Tags DROP name;
+ALTER TABLE Products ALTER COLUMN name RENAME TO label;
+ALTER TABLE Tags ALTER COLUMN name RENAME TO label;
 
