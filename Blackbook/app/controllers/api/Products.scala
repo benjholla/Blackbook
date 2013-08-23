8a913e2dd32ffd83c27f9cce55426ee8d356e4e4
Fifty-Nine <tim.prince@gmail.com>
2013-08-23 11:24:15 -0500

    For the debug and results though

--- Blackbook/app/controllers/api/Products.scala
+++ Blackbook/app/controllers/api/Products.scala
@@ -5,23 +5,36 @@ import play.api.mvc._
 import play.api.data._
 import play.api.libs.json._
 import play.api.libs.json.Json._
+import play.api.libs.json.Reads._
 import play.api.libs.json.Writes._
 import play.api.libs.functional.syntax._
 
 import models._
 
 object Products extends Controller { 
-  implicit val productWrites = Json.writes[Product]
-  implicit val productReads = Json.reads[Product]
+  implicit val productReads = reads[Product]
+  implicit val productWrites = writes[Product]
   
-  implicit val tagWrites = Json.writes[Tag]
-  implicit val tagReads = Json.reads[Tag]
+  implicit val tagReads = reads[Tag]
+  implicit val tagWrites = writes[Tag]
   
-  def products = Action { 
+  def all = Action { 
     Ok(toJson(Map(
       "status" -> toJson("OK"),
       "result" -> toJson(models.Product.all())
     )))
+  }
+  
+  def create = Action(parse.json) { request =>
+    ( request.body \ "name" ).asOpt[String].map { name =>
+      val result = models.Product.create(name)
+      Ok(toJson(Map(
+        "status" -> toJson("OK"),
+        "result" -> toJson(result)
+      )))
+    }.getOrElse { 
+      BadRequest("Missing parameter [name]")
+    }
   }
 
 }
