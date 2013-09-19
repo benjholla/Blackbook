504860f2eab175913406ec5307ff72b3436557ba
Ben Holland <benjholla@gmail.com>
2013-09-19 10:44:05 -0500

    Update 10.sql

--- conf/evolutions/default/10.sql
+++ conf/evolutions/default/10.sql
@@ -2,10 +2,13 @@
 
 # --- !Ups
 
+CREATE EXTENSION pgcrypto; # enabled crypto modules on postgres database
+
 ALTER TABLE Users ADD COLUMN HashedPassword text;
 UPDATE Users SET HashedPassword = crypt(Password, gen_salt('md5'));
 ALTER TABLE Users DROP COLUMN Password;
 
 # --- !Downs
 
+DROP EXTENSION pgcrypto; # disable crypto modules on postgres database
 ALTER TABLE Users DROP COLUMN HashedPassword;
